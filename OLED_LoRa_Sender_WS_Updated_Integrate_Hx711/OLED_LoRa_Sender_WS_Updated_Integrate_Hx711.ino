fc507629c01f034f988cf6e2539dbf1bba660389
Jenseng20 <jensengaither@gmail.com>
2020-02-01 23:13:28 -0500

    Create OLED_LoRa_Sender_WS_Updated_Integrate_Hx711.ino

---
+++ OLED_LoRa_Sender_WS_Updated_Integrate_Hx711/OLED_LoRa_Sender_WS_Updated_Integrate_Hx711.ino
@@ -0,0 +1,133 @@
+/********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/ttgo-lora32-sx1276-arduino-ide/
+*********/
+
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//Libraries for sensors
+#include <HX711.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//Define LoRa Band
+//433E6 for Asia
+//866E6 for Europe
+//915E6 for North America
+#define BAND 915E6
+
+//OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+//Define HX711 Pins
+#define HX711_DOUT 12 
+#define HX711_SCK 13
+
+//packet counter
+int counter = 0;
+
+//Initialize Oled
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+//Initialize HX711
+HX711 scale;
+
+void setup() {
+
+  //reset OLED display via software
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+
+  //initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(32,32);
+  display.print("LORA SENDER ");
+  display.display();
+  
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  
+  Serial.println("LoRa Sender Test");
+
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+  
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Initializing OK!");
+  display.setCursor(0,10);
+  display.print("LoRa Initializing OK!");
+  display.display();
+  delay(2000);
+
+  scale.begin(HX711_DOUT_PIN, HX711_SCK_PIN);
+  if (!scale.is_ready())
+  {
+    Serial.println("Starting Hx711 failed!");
+    display.clearDisplay();
+    display.setCursor(32,32);
+    display.print("Hx711");
+    display.setCursor(32,42);
+    display.print("Initializing");
+    display.setCursor(32,52);
+    display.print("Fail!");
+    display.display();
+  }
+}
+
+void loop() {
+   
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+  
+  display.clearDisplay();
+  display.setCursor(35,32);
+  display.println("LORA");
+  display.setCursor(35,40);
+  display.setTextSize(1);
+  display.print("Counter:");
+  display.setCursor(35,50);
+  display.print(counter);      
+  display.display();
+
+  counter++;
+  
+  delay(1000);
+}
