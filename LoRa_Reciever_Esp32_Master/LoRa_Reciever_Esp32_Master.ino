410160c86fb93413e2ae689b3a39963483accc90
Jenseng20 <jensengaither@gmail.com>
2020-02-20 20:23:22 -0500

    Update LoRa_Reciever_Esp32_Master.ino

--- LoRa_Reciever_Esp32_Master/LoRa_Reciever_Esp32_Master.ino
+++ LoRa_Reciever_Esp32_Master/LoRa_Reciever_Esp32_Master.ino
@@ -1,5 +1,5 @@
 /*********
-  Jensen Gaither
+  Jensen Gaither - Internet Of Bees
 *********/
 
 //Libraries for LoRa
@@ -14,8 +14,12 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
+//eeprom library
+#include <EEPROM.h>
+
 //Blynk Library
 #include <BlynkSimpleEsp32.h>
+#define BLYNK_PRINT Serial
 
 //define the pins used by the LoRa transceiver module
 #define SCK 5
@@ -31,6 +35,8 @@
 
 //blynk Variables
 const char auth[] = "rC8MTOyvwXUT8EzPTRW5ZWQyRRsuYxNB";
+const char server[] = "45.55.96.146";
+const int port = 443;
 
 //Wifi Variables
 const char ssid[] = "Team Donut";
@@ -59,51 +65,59 @@ long rawWht;
 long rawWeightLbs;
 long noTareWeightLbs;
 long weightLbs;
-long scaleFactor;
-long offset;
-long tareOffset;
+long scaleFactor = 1;
+long offset = 20;
+long tareOffset = 0;
 
 //cycle counter
 int counter;
+
+//Blynk Terminal variables
+WidgetTerminal terminal(V7);
+String terminalString;
 
 //create timer
 #define weatherInterval 900000L
 BlynkTimer timer;
 int weatherTimer = 1;
 
+/*****************************************************/
+/**************** Hive Data Functions ****************/
+/*****************************************************/
+
 void getHiveData()
 {
-    //received a packet
-    Serial.print("Received packet ");
+  //received a packet
+  Serial.print("Received packet ");
 
-    //read packet
-    while (LoRa.available()) 
-    {
-      loraData = LoRa.readString();
-      Serial.println(loraData);
-    }
+  //read packet
+  while (LoRa.available())
+  {
+    loraData = LoRa.readString();
+    Serial.println(loraData);
+  }
 
-    int rssi = LoRa.packetRssi();
-    Serial.print("with RSSI ");    
-    Serial.println(rssi);
+  int rssi = LoRa.packetRssi();
+  Serial.print("with RSSI ");
+  Serial.println(rssi);
 
-    tokenizeLoraString(loraData);
-    processWeight();
+  tokenizeLoraString(loraData);
+  processWeight();
 
-    Serial.println();
+  Serial.println();
 }
 
 void tokenizeLoraString(String loraString)
 {
-  char strBuffer[loraString.length()+1] = "";
-  loraString.toCharArray(strBuffer, loraString.length()+1); // example: "\"45.3\""
+  char strBuffer[loraString.length() + 1] = "";
+  loraString.toCharArray(strBuffer, loraString.length() + 1); // example: "\"45.3\""
   rawWht = atoi(strtok(strBuffer, "/"));
   hiveTempC = atof(strtok(NULL, "/"));
   hiveHumx = atof(strtok(NULL, "/"));
   counter = atoi(strtok(NULL, "/"));
 
-  hiveTempF = (hiveTempC * (9/5)) + 32;
-  
+  hiveTempF = (hiveTempC * (9 / 5)) + 32;
+
   Serial.print("Raw Weight: ");
   Serial.println(rawWht);
   Serial.print("Hive Temp F: ");
@@ -121,21 +135,25 @@ void processWeight()
   weightLbs = noTareWeightLbs - tareOffset;
 }
 
+/***********************************************************/
+/**************** Weather Webhook Functions ****************/
+/***********************************************************/
+
 void getWeatherData()
 {
-  if ((WiFi.status() == WL_CONNECTED)) 
-  { 
+  if ((WiFi.status() == WL_CONNECTED))
+  {
     HTTPClient http;
     http.begin(endpoint + key); //Specify the URL
     int httpCode = http.GET();  //Make the request
-    if (httpCode > 0) 
+    if (httpCode > 0)
     { //Check for the returning code
-        jsonPayload = http.getString();
-        Serial.println(httpCode);
-        Serial.println(jsonPayload);
-        parseWeatherData(jsonPayload);
+      jsonPayload = http.getString();
+      Serial.println(httpCode);
+      Serial.println(jsonPayload);
+      parseWeatherData(jsonPayload);
     }
-    else 
+    else
     {
       Serial.println("Error on HTTP request");
     }
@@ -149,78 +167,322 @@ void getWeatherData()
 
 void parseWeatherData(String jsonString)
 {
-  const size_t capacity = JSON_ARRAY_SIZE(1) + 2*JSON_OBJECT_SIZE(1) + 2*JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(14) + 440;
+  const size_t capacity = JSON_ARRAY_SIZE(1) + 2 * JSON_OBJECT_SIZE(1) + 2 * JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(14) + 440;
   DynamicJsonBuffer jsonBuffer(capacity);
-  
+
   const char* json = jsonString.c_str();
-  
+
   JsonObject& root = jsonBuffer.parseObject(json);
-  
+
   JsonObject& main = root["main"];
-  outsideTempK = main["temp"]; 
+  outsideTempK = main["temp"];
   outsideTempC = outsideTempK - 273.15;
-  outsideTempF = (outsideTempC * (9/5)) + 32;
-  outsidePressure = main["pressure"]; 
-  outsideHumx = main["humidity"]; 
+  outsideTempF = (outsideTempC * (9 / 5)) + 32;
+  outsidePressure = main["pressure"];
+  outsideHumx = main["humidity"];
   Serial.println(outsideTempF);
   Serial.println(outsidePressure);
   Serial.println(outsideHumx);
 }
 
+/***********************************************************/
+/**************** Blynk Data Push Functions ****************/
+/***********************************************************/
+
 void pushBlynkData()
 {
-    Blynk.virtualWrite(V0, counter);
-    Blynk.virtualWrite(V1, rawWht);
-    Blynk.virtualWrite(V2, hiveTempF);
-    Blynk.virtualWrite(V3, hiveHumx);
-    Blynk.virtualWrite(V4, outsideTempF);
-    Blynk.virtualWrite(V5, outsideHumx);
-    Blynk.virtualWrite(V6, outsidePressure); 
+  Blynk.virtualWrite(V0, counter);
+  Blynk.virtualWrite(V1, weightLbs);
+  Blynk.virtualWrite(V2, hiveTempF);
+  Blynk.virtualWrite(V3, hiveHumx);
+  Blynk.virtualWrite(V4, outsideTempF);
+  Blynk.virtualWrite(V5, outsideHumx);
+  Blynk.virtualWrite(V6, outsidePressure);
 }
 
-void setup() 
-{ 
+/****************************************************************/
+/**************** Miscellaneous System Functions ****************/
+/****************************************************************/
+
+void(* resetFunc) (void) = 0; //declare reset function @ address 0
+
+void writeEEPROM()
+{
+  int eeAddress = 0;
+  EEPROM.put(eeAddress, scaleFactor);
+  eeAddress += sizeof(scaleFactor);
+  EEPROM.put(eeAddress, offset);
+  eeAddress += sizeof(offset);
+  EEPROM.put(eeAddress, tareOffset);
+  eeAddress += sizeof(tareOffset);
+}
+
+void readEEPROM()
+{
+  int eeAddress = 0;
+  EEPROM.get(eeAddress, scaleFactor);
+  eeAddress += sizeof(scaleFactor);
+  EEPROM.get(eeAddress, offset);
+  eeAddress += sizeof(offset);
+  EEPROM.get(eeAddress, tareOffset);
+  eeAddress += sizeof(tareOffset);
+}
+
+
+
+/************************************************/
+/**************** Setup Function ****************/
+/************************************************/
+
+void setup()
+{
   //initialize Serial Monitor
   Serial.begin(115200);
   while (!Serial);
   Serial.println("LoRa Receiver Push to Blynk Test");
-  
+
   //Setup lora pins
   SPI.begin(SCK, MISO, MOSI, SS);//SPI LoRa pins
   LoRa.setPins(SS, RST, DIO0);//setup LoRa transceiver module
 
   //Initialize Lora
-  if (!LoRa.begin(BAND)) 
+  if (!LoRa.begin(BAND))
   {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
   Serial.println("LoRa Initializing OK!");
 
-  //Initialize Blynk
-  Blynk.begin(auth, ssid, pass);
-  if (!Blynk.connected()) 
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println("Connected to the WiFi network");
+
+  Blynk.config(auth, "blynk-cloud.com", 8080);
+
+  if (!Blynk.connect())
   {
     Serial.println("Starting Blynk failed!");
     while (1);
   }
   Serial.println("Blynk Initializing OK!");
 
+
   getWeatherData();
-  
+
   //Initialize Timers
-  weatherTimer = timer.setInterval(weatherInterval, getWeatherData); 
+  weatherTimer = timer.setInterval(weatherInterval, getWeatherData);
 }
 
-void loop() 
+/***********************************************/
+/**************** Loop Function ****************/
+/***********************************************/
+
+void loop()
 {
-  
+
   timer.run();
   Blynk.run();
   int packetSize = LoRa.parsePacket();
-  if (packetSize) 
+  if (packetSize)
   {
+    if (!Blynk.connected()) {
+      Serial.println("Blynk not connected");
+    }
     getHiveData();
     pushBlynkData();
   }
 }
+
+/**********************************************************/
+/**************** Blynk Terminal Functions ****************/
+/**********************************************************/
+
+BLYNK_WRITE(V7)
+{
+  terminalString = param.asStr();
+  if (String("reset") == terminalString)
+  {
+    terminal.println(" ");
+    terminal.println("Going down for reboot now!");
+    terminal.flush();
+    resetFunc();
+  }
+  if (String("debug hx711") == terminalString)
+  {
+    debugLoop(1);
+  }
+  if (String("debug si7020") == terminalString)
+  {
+    debugLoop(2);
+  }
+  if (String("debug weather") == terminalString)
+  {
+    debugLoop(3);
+  }
+  if (String("debug wifi") == terminalString)
+  {
+    debugLoop(4);
+  }
+  if (String("clear") == terminalString)
+  {
+    clearTerminal();
+  }
+  if (String("help") == terminalString || String("Help") == terminalString || String("HELP") == terminalString)
+  {
+    clearTerminal();
+    terminal.println("Help page 1 of 1");
+    terminal.println("COMMANDS:");
+    terminal.println(" ");
+    terminal.println("reset");
+    terminal.println("==> Resets device");
+    terminal.println(" ");
+    terminal.println("read");
+    terminal.println("==> Displays current hive info");
+    terminal.println(" ");
+    terminal.println("debug hx711");
+    terminal.println("==> Displays HX711 related debug info");
+    terminal.println(" ");
+    terminal.println("debug si7020");
+    terminal.println("==> Displays si7020 related debug info");
+    terminal.println(" ");
+    terminal.println("debug weather");
+    terminal.println("==> Displays weather related debug info");
+    terminal.println(" ");
+    terminal.println("1~val");
+    terminal.println("==> Sets scale factor to val");
+    terminal.println(" ");
+    terminal.println("2~val");
+    terminal.println("==> Sets scale offset to val");
+    terminal.println(" ");
+    terminal.println("3~val");
+    terminal.println("==> Sets scale tareOffset to val");
+    terminal.println(" ");
+    terminal.println("Text or call Jensen at 410-390-1670 for more help");
+    terminal.flush();
+  }
+  if (terminalString.indexOf('~') != -1)
+  {
+    terminal.println(" ");
+    terminal.println("In set function");
+    terminal.flush();
+    int stringLength = terminalString.length() + 1;
+    char strBuffer[stringLength] = "";
+    terminalString.toCharArray(strBuffer, stringLength);
+    int operation = atoi(strtok(strBuffer, "\"~"));
+    float value = atof(strtok(NULL, "~"));
+
+    if (operation == 1)
+    {
+      scaleFactor = value;
+      writeEEPROM();
+      terminal.println("==> scaleFactor written");
+      terminal.flush();
+    }
+
+    if (operation == 2)
+    {
+      offset = value;
+      writeEEPROM();
+      terminal.println("==> offset written");
+      terminal.flush();
+    }
+    if (operation == 3)
+    {
+      tareOffset = value;
+      writeEEPROM();
+      terminal.println("==> tareOffset written");
+      terminal.flush();
+    }
+  }
+}
+
+void debugLoop(int x)
+{
+  if (x == 1)
+  {
+    clearTerminal();
+    terminal.println("Current states of HX711 related variables:");
+    terminal.print("rawWeight = ");
+    terminal.println(rawWht);
+    terminal.print("rawWeightLbs = ");
+    terminal.println(rawWeightLbs);
+    terminal.print("noTareWeightLbs = ");
+    terminal.println(noTareWeightLbs);
+    terminal.print("weight = ");
+    terminal.println(weightLbs);
+    terminal.print("scaleFactor = ");
+    terminal.println(scaleFactor);
+    terminal.print("offset = ");
+    terminal.println(offset);
+    terminal.print("tareOffset = ");
+    terminal.println(tareOffset);
+    terminal.flush();
+  }
+
+  if (x == 2)
+  {
+    clearTerminal();
+    terminal.println("Current states of si7020 related variables:");
+    terminal.print("hiveTempC = ");
+    terminal.println(hiveTempC);
+    terminal.print("hiveTempF = ");
+    terminal.println(hiveTempF);
+    terminal.print("hiveHumx = ");
+    terminal.println(hiveHumx);
+    terminal.flush();
+  }
+
+  if (x == 3)
+  {
+    clearTerminal();
+    terminal.println("Current states of weather related variables:");
+    terminal.print("outsideTempK = ");
+    terminal.println(outsideTempK);
+    terminal.print("outsideTempC = ");
+    terminal.println(outsideTempC);
+    terminal.print("outsideTempF = ");
+    terminal.println(outsideTempF);
+    terminal.print("outsideHumx = ");
+    terminal.println(outsideHumx);
+    terminal.print("outsidePressure = ");
+    terminal.println(outsidePressure);
+    terminal.flush();
+  }
+  if (x == 4)
+  {
+    clearTerminal();
+    terminal.println("Current states of Wifi related variables:");
+    terminal.print("SSID of network = ");
+    terminal.println(WiFi.SSID());
+    terminal.print("RSSI of network = ");
+    terminal.println(WiFi.RSSI());
+    terminal.flush();
+  }
+}
+
+void clearTerminal()
+{
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.println(" ");
+  terminal.flush();
+}
+
+/*******************************************************/
+/**************** BLYNK_WRITE Functions ****************/
+/*******************************************************/
